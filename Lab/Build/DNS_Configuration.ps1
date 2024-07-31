67ad4aa0f2fd29d9db71bfecdc0c42149dc42bcb
VGSandz <venugopal.bhamidipati@sap.com>
2024-07-31 17:39:42 +0530

    DNS updates

--- Lab/Build/DNS_Configuration.ps1
+++ Lab/Build/DNS_Configuration.ps1
@@ -1,2 +1,15 @@
 #Set the DNS server to Listen only on the NIC IP
+# Replace the IP for which the DNS accepts the incoming. This will also disable listening on all IPs.
+# update accordingly before calling the command.
+dnscmd /ResetListenAddresses 192.168.1.10
+
+#Create a reverse lookup zone.
+# Network is 192.169.1.0
+# -DynamicUpdate Secure , Allows only Secure Updates.
+# -ReplicationScope Forest , Replication is applicable to All Domain Controllers in the Forest.
+# update accordingly before calling the command.
+Add-DnsServerPrimaryZone -DynamicUpdate Secure -Name 168.192.in-addr.arpa -ReplicationScope Forest
+
 #Restart the DNS service after the change.
+Restart-Service DNS -Verbose
+
