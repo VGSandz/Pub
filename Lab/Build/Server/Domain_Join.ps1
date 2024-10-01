024e22d4f91f9cf3cc9eedb1254680865c5d82ea
VGSandz <64747937+VGSandz@users.noreply.github.com>
2024-10-01 22:55:01 +0530

    Update Domain_Join.ps1

--- Lab/Build/Server/Domain_Join.ps1
+++ Lab/Build/Server/Domain_Join.ps1
@@ -1,7 +1,16 @@
-#Join the Server to Domain.
+#Join the Server to Domain. [Domain Controller Promotion]
 #This command is as simple as it gets because the server would be promoted to a Domain Controller. 
 #There is no necessary to keep the server into a specific OU.
 #DNS lookup of the domain has to work for this command to execute properly.
 
 $domain_name = "labone.local"
 Add-Computer -DomainName $domain_name -Credential (Get-Credential) -Restart
+
+#Join the Server to Domain. [Workstations,Servers, etc]
+#Get the Domain.
+$domain_name = "labone.local" ## Custom Domain
+#get the OU information.
+$ou = "OU=Servers,OU=Resources,DC=labone,DC=local" # Update OU as required, this attribute is the DN of the OU in Attributes.
+#Join the server to the Domain and place it in the respective OU.
+#This would prompt for Credentials which should be of your domain to which the server is about to be joined.
+Add-Computer -DomainName $domain_name -OUPath $ou -Credential (Get-Credential) -Restart
